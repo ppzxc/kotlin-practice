95b7c1a3132eb92a857b464e24fa08e55fe678fc
ppzxc <cjh8487@naver.com>
2022-12-02 14:10:12 +0900

    2022-12-02

--- practice01-grammar/src/main/kotlin/org/example/Grammar01.kt
+++ practice01-grammar/src/main/kotlin/org/example/Grammar01.kt
@@ -1,5 +1,10 @@
 package org.example
 
 fun main(args: Array<String>) {
+    val tt: Any? = null
 
+//    tt?.let { println("NOT NULL") } ?: run { println("NULL") }
+
+    val result = tt?.toString() ?: "FF"
+    println(result)
 }
